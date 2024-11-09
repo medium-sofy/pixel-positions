1d014a570acaa51ff91e9628853fb3dd362de062
Mohamed Sofy <mediumsofy@gmail.com>
2024-11-09 19:36:36 +0200

    Rename welcome.blade to index.blade

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -3,39 +3,34 @@
         <section class="text-center pt-6">
             <h1 class="font-bold text-4xl">Let's Find Your Next Job</h1>
             <form action="">
-                <input type="text" name="" id="" placeholder="Web Developer..." class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full mt-6 max-w-xl">
+                <input type="text" name="" id="" placeholder="Web Developer..."
+                    class="rounded-xl bg-white/5 border-white/10 px-5 py-4 w-full mt-6 max-w-xl">
             </form>
         </section>
         <section class="pt-10">
             <x-section-heading>Featured Jobs</x-section-heading>
             <div class="grid xl:grid-cols-3 gap-8 mt-6">
-                <x-job-card></x-job-card>
-                <x-job-card></x-job-card>
-                <x-job-card></x-job-card>
+                @foreach ($featuredJobs as $job)
+                    <x-job-card :$job />
+                @endforeach
             </div>
         </section>
 
         <section>
             <x-section-heading>Tags</x-section-heading>
             <div class="mt-6 space-x-1 flex justify-center flex-wrap">
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
+                @foreach ($tags as $tag)
+                    <x-tag :$tag />
+                @endforeach
+
             </div>
         </section>
         <section>
             <x-section-heading>Recent Jobs</x-section-heading>
             <div class="mt-6 space-y-6">
-                <x-job-card-wide/>
-                <x-job-card-wide/>
-                <x-job-card-wide/>
+                @foreach ($jobs as $job)
+                    <x-job-card-wide :$job />
+                @endforeach
                 <x-panel>Hello there</x-panel>
             </div>
         </section>
