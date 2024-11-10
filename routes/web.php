dede49c7d29ee5e4122d1c2c7d031ef6fa993f3b
Mohamed Sofy <mediumsofy@gmail.com>
2024-11-10 16:27:27 +0200

    Register route for viewing jobs with certain tags

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
+use App\Http\Controllers\TagController;
 use App\Http\Controllers\SearchController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\RegisteredUserController;
 
 Route::get('/', [JobController::class, 'index']);
 Route::get('/search', SearchController::class);
+Route::get('/tags/{tag:name}', TagController::class);
 Route::middleware('guest')->group(function () {
     Route::get('/register', [RegisteredUserController::class, 'create']);
     Route::post('/register', [RegisteredUserController::class, 'store']);
