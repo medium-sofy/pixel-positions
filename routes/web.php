78ce0ce69c5b6d01ddd52c84fe80c04791791bba
Mohamed Sofy <mediumsofy@gmail.com>
2024-11-10 08:12:36 +0200

    Prevent signed in user from viewing login and registration form

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,15 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\JobController;
-use App\Http\Controllers\SessionController;
 use App\Http\Controllers\RegisteredUserController;
+use App\Http\Controllers\SessionController;
+use Illuminate\Support\Facades\Route;
 
 Route::get('/', [JobController::class, 'index']);
-
-Route::get('/register', [RegisteredUserController::class,'create']);
-Route::post('/register', [RegisteredUserController::class,'store']);
-
-Route::get('/login', [SessionController::class,'create']);
-Route::post('/login', [SessionController::class,'store']);
-Route::delete('/logout', [SessionController::class,'destroy']);
+Route::middleware('guest')->group(function () {
+    Route::get('/register', [RegisteredUserController::class, 'create']);
+    Route::post('/register', [RegisteredUserController::class, 'store']);
+    Route::get('/login', [SessionController::class, 'create']);
+    Route::post('/login', [SessionController::class, 'store']);
+});
+Route::delete('/logout', [SessionController::class, 'destroy']);
