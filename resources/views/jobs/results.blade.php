7d30c5e3b70b114e231eb7a7880bb4c2e7984aba
Mohamed Sofy <mediumsofy@gmail.com>
2024-11-10 18:27:07 +0200

    Display number of results in search page

--- resources/views/jobs/results.blade.php
+++ resources/views/jobs/results.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <x-page-heading>Results:</x-page-heading>
+    <x-page-heading>Showing {{ count($jobs) }} results for: <span class="text-yellow-300">{{ request('q') }}</span></x-page-heading>
     <div class="space-y-6">
         @foreach ($jobs as $job)
             <x-job-card-wide :$job />
