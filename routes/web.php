ec6051f9319b0a1550b1da1d604c991f340849ec
Mohamed Sofy <mediumsofy@gmail.com>
2024-11-10 16:31:36 +0200

    Register routes for posting a new job

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,13 @@ use App\Http\Controllers\SessionController;
 use App\Http\Controllers\RegisteredUserController;
 
 Route::get('/', [JobController::class, 'index']);
+
+Route::get('/jobs/create', [JobController::class, 'create'])->middleware('auth');
+Route::post('/jobs', [JobController::class, 'store'])->middleware('auth');
+
 Route::get('/search', SearchController::class);
 Route::get('/tags/{tag:name}', TagController::class);
+
 Route::middleware('guest')->group(function () {
     Route::get('/register', [RegisteredUserController::class, 'create']);
     Route::post('/register', [RegisteredUserController::class, 'store']);
